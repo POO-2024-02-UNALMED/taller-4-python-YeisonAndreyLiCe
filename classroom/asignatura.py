a76144ef4d33e38d535e8f674653bd33885056f6
Yeison Liscano <yliscanoc@unal.edu.co>
2024-11-24 12:38:02 -0500

    fix errors

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,12 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return (
+            f"{self._nombre} {self._salon}"
+            if self._salon
+            else  f"{self._nombre} remoto"
+        )
